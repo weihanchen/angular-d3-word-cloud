4d14ae8e5de5faee659b9d8f9e460f0ef592757d
weihanchen <willhanchen@gmail.com>
2016-08-30 16:34:04 +0800

    use mocha to report test result

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['mocha'],
 
 
         // web server port
@@ -51,15 +51,15 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // browsers: ['Chrome'],
+        browsers: ['Chrome'],
 
         // run with travis ci
-        customLaunchers: {
-            Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
-        },
+        // customLaunchers: {
+        //     Chrome_travis_ci: {
+        //         base: 'Chrome',
+        //         flags: ['--no-sandbox']
+        //     }
+        // },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
@@ -69,8 +69,8 @@ module.exports = function(config) {
         // how many browser should be started simultaneous
         concurrency: Infinity
     }
-    if (process.env.TRAVIS) {
-        configuration.browsers = ['Chrome_travis_ci'];
-    }
+    // if (process.env.TRAVIS) {
+    //     configuration.browsers = ['Chrome_travis_ci'];
+    // }
     config.set(configuration)
 }
